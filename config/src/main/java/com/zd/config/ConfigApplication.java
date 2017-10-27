6d5482f8eb82648299c1131842e04c27462214bf
江俊升 <857012499@qq.com>
2017-10-27 09:50:15 +0800

    配置中心加入注册服务

--- config/src/main/java/com/zd/config/ConfigApplication.java
+++ config/src/main/java/com/zd/config/ConfigApplication.java
@@ -3,14 +3,14 @@ package com.zd.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@Configuration
 @EnableAutoConfiguration
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
 }
