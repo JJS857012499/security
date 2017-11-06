9401bb80d5ddf9ca53dd0dbd544393fb85d30458
江俊升 <857012499@qq.com>
2017-11-06 18:40:21 +0800

    可配参数

---
+++ admin/src/main/java/com/zd/admin/AdminApplication.java
@@ -0,0 +1,13 @@
+package com.zd.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+}
