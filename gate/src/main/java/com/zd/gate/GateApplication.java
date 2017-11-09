efbae211e9a10133f0644c1e0b554732288cbb6d
江俊升 <857012499@qq.com>
2017-11-09 15:54:59 +0800

    gate init...

---
+++ gate/src/main/java/com/zd/gate/GateApplication.java
@@ -0,0 +1,16 @@
+package com.zd.gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy // 使用@EnableZuulProxy来开启Zuul的支持，如果你不想使用Zuul提供的Filter和反向代理的功能的话，此处可以使用@EnableZuulServer注解
+@EnableDiscoveryClient
+public class GateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateApplication.class, args);
+	}
+}
